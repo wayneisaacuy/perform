fixed basis issue. working on deim issue for internal computation

perform/rom/projection_rom/linear_proj_rom/linear_proj_rom.py
@@ -36,12 +36,16 @@ class LinearProjROM(ProjectionROM):
         super().__init__(model_idx, rom_domain, sol_domain, solver)
 
         # load and check trial basis
-        self.trial_basis = np.load(rom_domain.model_files[self.model_idx])
+        if isinstance(rom_domain.model_files[self.model_idx], np.ndarray):
+            self.trial_basis = rom_domain.model_files[self.model_idx]
+        else:
+            self.trial_basis = np.load(rom_domain.model_files[self.model_idx])
         num_vars_basis_in, num_cells_basis_in, num_modes_basis_in = self.trial_basis.shape
 
         assert num_vars_basis_in == self.num_vars, (
             "Basis at "
-            + rom_domain.model_files[self.model_idx]
+            + str(self.model_idx)
+            #+ rom_domain.model_files[self.model_idx]
             + " represents a different number of variables "
             + "than specified by modelVarIdxs ("
             + str(num_vars_basis_in)
@@ -51,7 +55,8 @@ class LinearProjROM(ProjectionROM):
         )
         assert num_cells_basis_in == sol_domain.mesh.num_cells, (
             "Basis at "
-            + rom_domain.model_files[self.model_idx]
+            + str(self.model_idx)
+            #+ rom_domain.model_files[self.model_idx]
             + " has a different number of cells ("
             + str(num_cells_basis_in)
             + " != "
@@ -60,7 +65,8 @@ class LinearProjROM(ProjectionROM):
         )
         assert num_modes_basis_in >= self.latent_dim, (
             "Basis at "
-            + rom_domain.model_files[self.model_idx]
+            + str(self.model_idx)
+            #+ rom_domain.model_files[self.model_idx]
             + " must have at least "
             + str(self.latent_dim)
             + " modes ("

perform/rom/rom_domain.py
@@ -191,6 +191,11 @@ class RomDomain:
             
             #breakpoint()
         else:
+            
+            # not yet implemented for ROMs which are not linear
+            if self.rom_method[-7:] == "tfkeras":
+                raise Exception('Automated computation of basis and DEIM sampling points not yet supported for nonlinear ROMs.')
+            
             # compute basis and scaling profiles
             spatial_modes, cent_file, norm_sub_file, norm_fac_file = gen_ROMbasis(self.model_dir,
                                                                                   solver.dt, 
@@ -227,7 +232,7 @@ class RomDomain:
                 # Set up adaptive basis, if necessary
                 
                 if self.adaptiveROM:
-                    raise Exception('Need to clean out dependencies on loading the rom basis and also hyperred basis')
+                    raise Exception('Need to clean out dependencies on loading the rom basis and also hyperred basis.remember hyperred basis = basis')
                     # check that the time scheme is bdf
                     assert solver.time_scheme == "bdf", "Adaptive basis requires implicit time-stepping"
                     
@@ -241,7 +246,10 @@ class RomDomain:
                     
                     ROMDEIM_basis_same = 1
                     for idx in range(self.num_models):
-                        rom_basis = np.load(self.model_files[idx])
+                        if isinstance(self.model_files[idx], np.ndarray):
+                            rom_basis = self.model_files[idx]
+                        else:
+                            rom_basis = np.load(self.model_files[idx])
                         rom_basis = rom_basis[:,:,:self.latent_dims[idx]]
                         deim_basis = np.load(self.hyper_reduc_files[idx])
                         deim_basis = deim_basis[:,:,:self.hyper_reduc_dims[idx]]