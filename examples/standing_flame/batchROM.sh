158725bb35e2ba6105640637f57bc06e02488d9b
Wayne Isaac Uy <wayneisaacuy@MacBook-Pro-de-Wayne.local>
2021-09-01 18:14:05 -0400

    add batchROM

---
+++ examples/standing_flame/batchROM.sh
@@ -0,0 +1,18 @@
+module load python/intel/3.8.6
+
+export PATH="$HOME/uni/apro/mlib:$PATH"
+
+dt=1e-10
+latentDims=2
+updateFreq=1000
+
+for initWindowSize in 15 25 50 100 250
+do
+for adaptWindowSize in 3 5 7 10 20 30 50
+do
+
+pySLURM.py "../../perform/driver.py /scratch/work/peherstorfer/wtu1/perform/examples/standing_flame --calc_rom 1 --dt $dt --latent_dims $latentDims --init_window_size $initWindowSize --adapt_window_size $adaptWindowSize --adapt_update_freq $updateFreq"
+
+done
+done
+
