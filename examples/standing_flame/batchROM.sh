c0c0270d773984aaf84da6d56b88a022f5d31142
Wayne Isaac Uy <wayneisaacuy@MacBook-Pro-de-Wayne.local>
2021-09-04 18:15:04 -0400

    edited bash batch script for ROM

--- examples/standing_flame/batchROM.sh
+++ examples/standing_flame/batchROM.sh
@@ -2,19 +2,20 @@ module load python/intel/3.8.6
 
 export PATH="$HOME/uni/apro/mlib:$PATH"
 
-dt=1e-8
-nrsteps=4000
+dt=1e-10
+outskip=5
+nrsteps=20000
 #latentDims=2
 updateFreq=1000
 
-for latentDims in 2 3
+for latentDims in 2 3 4 5
 do
-for initWindowSize in 5 15 25 50 100
+for initWindowSize in 15 25 50 100 250 #5 15 25 50 100
 do
-for adaptWindowSize in 5 10 20 30 50
+for adaptWindowSize in 5 10 20 30 50 #5 10 20 30 50
 do
 
-pySLURM.py "../../perform/driver.py /scratch/work/peherstorfer/wtu1/perform/examples/standing_flame --calc_rom 1 --dt $dt --nrsteps $nrsteps --latent_dims $latentDims --init_window_size $initWindowSize --adapt_window_size $adaptWindowSize --adapt_update_freq $updateFreq"
+pySLURM.py "../../perform/driver.py /scratch/work/peherstorfer/wtu1/perform/examples/standing_flame --calc_rom 1 --dt $dt --nrsteps $nrsteps --latent_dims $latentDims --init_window_size $initWindowSize --adapt_window_size $adaptWindowSize --adapt_update_freq $updateFreq --out_skip $outskip"
 
 done
 done
