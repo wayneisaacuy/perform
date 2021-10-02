371b6c710b1cf2cc616d4040b4a841d0a63ba98a
Wayne Isaac Uy <wayneisaacuy@MacBook-Pro-de-Wayne.local>
2021-10-02 07:16:23 -0400

    edit batchFOM

--- examples/standing_flame/batchFOM.sh
+++ examples/standing_flame/batchFOM.sh
@@ -2,7 +2,8 @@ module load python/intel/3.8.6
 
 export PATH="$HOME/uni/apro/mlib:$PATH"
 
-dt=1.0e-08
-nrsteps=4000
+dt=1.0e-09
+nrsteps=40000
+out_skip=10
 
-pySLURM.py "../../perform/driver.py /scratch/work/peherstorfer/wtu1/perform/examples/standing_flame --calc_rom 0 --dt $dt --nrsteps $nrsteps"
+pySLURM.py "../../perform/driver.py /scratch/work/peherstorfer/wtu1/perform/examples/standing_flame --calc_rom 0 --dt $dt --nrsteps $nrsteps --out_skip $out_skip"
