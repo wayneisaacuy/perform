64a5af0676b86dd1ed45b0618ce139210320b725
Wayne Isaac Uy <wayneisaacuy@MacBook-Pro-de-Wayne.local>
2021-10-04 20:08:43 -0400

    set use ADEIM to 1

--- examples/standing_flame/batchROM.sh
+++ examples/standing_flame/batchROM.sh
@@ -13,7 +13,7 @@ for latentDims in 5 6 7 8 9 10
 do
 for initWindowSize in 15 25 50 100 250 500 1000
 do
-for adaptWindowSize in 11 12 13 14 #15 25 50 75 100 250 500 1000
+for adaptWindowSize in 11 12 13 14 15 25 50 75 100 250 500 1000
 do
 
 pySLURM.py "../../perform/driver.py /scratch/work/peherstorfer/wtu1/perform/examples/standing_flame --calc_rom 1 --dt $dt --nrsteps $nrsteps --latent_dims $latentDims --init_window_size $initWindowSize --adapt_window_size $adaptWindowSize --adapt_update_freq $updateFreq --ADEIM_update $useADEIM --use_FOM $use_FOM --out_skip $outskip"
