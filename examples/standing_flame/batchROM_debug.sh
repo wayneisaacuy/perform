52df6adb7bde680b9e15fb917561638df1123a7b
Wayne Isaac Uy <wayneisaacuy@MacBook-Pro-de-Wayne.local>
2021-10-16 20:45:26 -0400

    fix typo

--- examples/standing_flame/batchROM_debug.sh
+++ examples/standing_flame/batchROM_debug.sh
@@ -14,7 +14,7 @@ adaptWindowSize=9
 
 numrescomp=2048
 
-pySLURM.py "../../perform/driver.py /scratch/work/peherstorfer/wtu1/perform/examples/standing_flame --calc_rom 1 --dt $dt --nrsteps $nrsteps --latent_dims $latentDims --init_window_size $initWindowSize --adapt_window_size $adaptWindowSize --sampling_update_freq 200*$adaptevery --ADEIM_update $useADEIM --use_FOM $use_FOM --adapt_every $adaptevery --num_residual_comp $numrescomp"
+pySLURM.py "../../perform/driver.py /scratch/work/peherstorfer/wtu1/perform/examples/standing_flame --calc_rom 1 --dt $dt --nrsteps $nrsteps --latent_dims $latentDims --init_window_size $initWindowSize --adapt_window_size $adaptWindowSize --sampling_update_freq $(( 200*adaptevery )) --ADEIM_update $useADEIM --use_FOM $use_FOM --adapt_every $adaptevery --num_residual_comp $numrescomp"
 
 #for learn_rate in 0.75 1.1 1.5 #2 5 10 25 50 100 #5e-01 1e-01
 #do
