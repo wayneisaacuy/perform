64effaf3e73b4a5bb92f06e1b0fe76fb457cf135
Wayne Isaac Uy <wayneisaacuy@MacBook-Pro-de-Wayne.local>
2021-11-01 23:26:30 -0400

    change parameters

--- examples/standing_flame/batchROM.sh
+++ examples/standing_flame/batchROM.sh
@@ -8,7 +8,7 @@ nrsteps=40000
 # useADEIM=ADEIM
 use_FOM=0
 outskip=10   
-useLineSearch=1
+useLineSearch=0
 
 for latentDims in 8 9 # 4 5 6 7 8 9 10 11
 do
@@ -16,11 +16,11 @@ for initWindowSize in 12 15 # 12 15 25 50 100
 do
 for adaptWindowSize in $(( latentDims+1 )) 11 13 15 # 12 14 7 25 50 75 100 latentDims + 1
 do
-for adaptevery in 2 3 4 5 10 100
+for adaptevery in 2 3 4 5 # 10 100
 do
 for useADEIM in AODEIM AFDEIM ADEIM
 do
-for numrescomp in 1750 1024 # 512 256 128
+for numrescomp in 2048 # 1750 1024 # 512 256 128
 do
 for multiplier in 1 # 5 10 50
 do
