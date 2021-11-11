455dd56551f647aff85cbc1ba4d91446cadb7e5a
Wayne Isaac Uy <wayneisaacuy@MacBook-Pro-de-Wayne.local>
2021-11-11 00:07:31 -0500

    edit parameters for transient flame

--- examples/transient_flame/batchROM.sh
+++ examples/transient_flame/batchROM.sh
@@ -4,21 +4,31 @@ export PATH="$HOME/uni/apro/mlib:$PATH"
 
 dt=5.0e-08
 nrsteps=14000
-updateFreq=1000
-useADEIM=0
 use_FOM=1
 outskip=10   
+useLineSearch=0
 
-for latentDims in 5 6 7 8 9 10
+for latentDims in 5 6 7 8 9 10 11
 do
-for initWindowSize in 15 25 50 100 250
+for initWindowSize in 12 15
 do
-for adaptWindowSize in 11 12 13 14 15 25 50 75 100 250
+for adaptWindowSize in $(( latentDims+1 )) 11 13 15
+do
+for adaptevery in 1 2 3 4
+do
+for useADEIM in POD
+do
+for numrescomp in 2048
+do
+for multiplier in 1
 do
 
-pySLURM.py "../../perform/driver.py /scratch/work/peherstorfer/wtu1/perform/examples/transient_flame --calc_rom 1 --dt $dt --nrsteps $nrsteps --latent_dims $latentDims --init_window_size $initWindowSize --adapt_window_size $adaptWindowSize --adapt_update_freq $updateFreq --ADEIM_update $useADEIM --use_FOM $use_FOM --out_skip $outskip"
+pySLURM.py "../../perform/driver.py /scratch/work/peherstorfer/wtu1/perform/examples/transient_flame --calc_rom 1 --dt $dt --nrsteps $nrsteps --latent_dims $latentDims --init_window_size $initWindowSize --adapt_window_size $adaptWindowSize --sampling_update_freq $(( multiplier*adaptevery )) --ADEIM_update $useADEIM --use_FOM $use_FOM --out_skip $outskip --adapt_every $adaptevery --num_residual_comp $numrescomp --use_line_search $useLineSearch" 
 
 done
 done
 done
-
+done
+done
+done
+done
