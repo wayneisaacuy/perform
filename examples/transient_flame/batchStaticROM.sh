333198b538eded5c498b236cf9d598b948dbb1e3
Wayne Isaac Uy <wayneisaacuy@MacBook-Pro-de-Wayne.local>
2021-12-09 22:54:07 -0500

    add static rom for transient flame

--- examples/transient_flame/batchStaticROM.sh
+++ examples/transient_flame/batchStaticROM.sh
@@ -2,12 +2,12 @@ module load python/intel/3.8.6
 
 export PATH="$HOME/uni/apro/mlib:$PATH"
 
-dt=5.0e-08
-nrsteps=14000
-snapskip=20
+dt=1.0e-09
+nrsteps=35000
+snapskip=50
 out_skip=10
 
-for latentDims in 3 5 7 10 15 20 25 50
+for latentDims 5 6 7 8 9
 do
 
 pySLURM.py "../../perform/driver.py /scratch/work/peherstorfer/wtu1/perform/examples/transient_flame --calc_rom 1 --dt $dt --nrsteps $nrsteps --latent_dims $latentDims --adaptive 0 --init_window_size $nrsteps --initbasis_snap_skip $snapskip --out_skip $out_skip"
